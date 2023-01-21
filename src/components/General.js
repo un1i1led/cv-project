652e608d6f63f90849e64e45aba4cddfc3198f9c
gil <gsqwow@gmail.com>
2023-01-20 23:01:57 -0700

    Rename inputs

--- src/components/General.js
+++ src/components/General.js
@@ -3,41 +3,29 @@ import React, { Component } from 'react';
 export default class General extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            name: '',
-            lastName: '',
-            title: '',
-            number: '',
-            email: '',
-            location: '',
-        }
-    }
-
-    changeState = (state, value) => {
-        this.setState({[state]: value});
     }
 
     render() {
         return (
             <div>
                 <div className='fname'>
-                    <input type='text' id='fname' name='fname' onInput={() => {this.changeState('name', 
-                    document.querySelector('#fname').value)}}></input>
+                    <input type='text' id='f-name' placeholder='Name' onInput={() => {this.props.updateState('name', 
+                    document.querySelector('#f-name').value)}}></input>
                 </div>
                 <div className='lname'>
-                    <input type='text' id='fname' name='fname'></input>
+                    <input type='text' id='l-name' placeholder='Last Name'></input>
                 </div>
                 <div className='title'>
-                    <input type='text' id='fname' name='fname'></input>
+                    <input type='text' id='job-title' placeholder='Job Title'></input>
                 </div>
                 <div className='number'>
-                    <input type='text' id='fname' name='fname'></input>
+                    <input type='text' id='p-num' placeholder='Phone Number'></input>
                 </div>
                 <div className='email'>
-                    <input type='text' id='fname' name='fname'></input>
+                    <input type='text' id='email' placeholder='Email'></input>
                 </div>
                 <div className='location'>
-                    <input type='text' id='fname' name='fname'></input>
+                    <input type='text' id='location' placeholder='Location'></input>
                 </div>
             </div>
         );
