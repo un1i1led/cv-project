4caf786a6f2605ec27f933623047ec5dcff810e6
gil <gsqwow@gmail.com>
2023-01-21 00:15:34 -0700

    Add display general details functionality

--- src/components/Display.js
+++ src/components/Display.js
@@ -9,6 +9,11 @@ export default class Display extends Component {
         return(
             <div>
                 <p>{this.props.state.name}</p>
+                <p>{this.props.state.lastName}</p>
+                <p>{this.props.state.title}</p>
+                <p>{this.props.state.number}</p>
+                <p>{this.props.state.email}</p>
+                <p>{this.props.state.location}</p>
             </div>
         )
     }
--- src/components/General.js
+++ src/components/General.js
@@ -13,19 +13,24 @@ export default class General extends Component {
                     document.querySelector('#f-name').value)}}></input>
                 </div>
                 <div className='lname'>
-                    <input type='text' id='l-name' placeholder='Last Name'></input>
+                    <input type='text' id='l-name' placeholder='Last Name' onInput={() => {this.props.updateState('lastName', 
+                    document.querySelector('#l-name').value)}}></input>
                 </div>
                 <div className='title'>
-                    <input type='text' id='job-title' placeholder='Job Title'></input>
+                    <input type='text' id='job-title' placeholder='Job Title' onInput={() => {this.props.updateState('title', 
+                    document.querySelector('#job-title').value)}}></input>
                 </div>
                 <div className='number'>
-                    <input type='text' id='p-num' placeholder='Phone Number'></input>
+                    <input type='text' id='p-num' placeholder='Phone Number' onInput={() => {this.props.updateState('number', 
+                    document.querySelector('#p-num').value)}}></input>
                 </div>
                 <div className='email'>
-                    <input type='text' id='email' placeholder='Email'></input>
+                    <input type='text' id='email' placeholder='Email' onInput={() => {this.props.updateState('email', 
+                    document.querySelector('#email').value)}}></input>
                 </div>
                 <div className='location'>
-                    <input type='text' id='location' placeholder='Location'></input>
+                    <input type='text' id='location' placeholder='Location' onInput={() => {this.props.updateState('location', 
+                    document.querySelector('#location').value)}}></input>
                 </div>
             </div>
         );
