813fb76c15fef8ad6771ed2109eeba5a2f59f323
gil <gsqwow@gmail.com>
2023-01-20 23:01:17 -0700

    Add props to General invoke

--- src/components/Creator.js
+++ src/components/Creator.js
@@ -4,16 +4,13 @@ import General from './General';
 export default class Creator extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            
-        }
     }
 
     render() {
         return(
             <div>
                 <h2>General Details</h2>
-                <General/>
+                <General state={this.props.state} updateState={this.props.updateState}/>
             </div>
         );
     }
