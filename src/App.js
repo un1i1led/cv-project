2b3d94f51b8af6e58af1cba9b94a14f608e09590
gil <gsqwow@gmail.com>
2023-01-20 23:00:06 -0700

    Reconstruct to class and add state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import Creator from './components/Creator';
+import Display from './components/Display';
 import uniqid from 'uniqid';
 
-function App() {
-  return (
-    <div className="App">
-      <Creator/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        name: '',
+        lastName: '',
+        title: 'Software Engineer',
+        number: '',
+        email: '',
+        location: 'San Diego, US',
+    }
+
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState = (state, value) => this.setState({[state]: value});
+
+  render() {
+    return(
+      <div>
+        <Creator state={this.state} updateState={this.updateState}/>
+        <Display state={this.state}/>
+      </div>
+    )
+  }
 }
 
 export default App;
