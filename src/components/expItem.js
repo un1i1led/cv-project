89339c4ef048f6318ea5075ee9f668d6ca2c512b
gil <gsqwow@gmail.com>
2023-01-27 23:10:53 -0700

    Add expItem factory

---
+++ src/components/expItem.js
@@ -0,0 +1,19 @@
+export const expItem = (id) => {
+    let position = '';
+    let company = '';
+    let city = '';
+    let from = '';
+    let to = '';
+    let description = '';
+
+
+    return {
+        id,
+        position,
+        company,
+        city,
+        from,
+        to,
+        description
+    }
+}
