81e9401be390aee618309752644118fbc223dfc3
gil <gsqwow@gmail.com>
2023-01-30 03:51:35 -0700

    Add education item factory

---
+++ src/components/eduItem.js
@@ -0,0 +1,16 @@
+export const eduItem = (id) => {
+    let course = '';
+    let university = '';
+    let start = '';
+    let end = '';
+    let description = '';
+
+    return {
+        id, 
+        course,
+        university,
+        start,
+        end,
+        description
+    }
+}
