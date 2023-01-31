5f27a6f4860755b6cbae2a66e54e6b0cf0d03e62
gil <gsqwow@gmail.com>
2023-01-30 18:46:18 -0700

    Add education & experience rendering

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import DisplayExp from './DisplayExp';
 
 export default class Display extends Component {
     constructor(props) {
@@ -8,12 +9,18 @@ export default class Display extends Component {
     render() {
         return(
             <div>
-                <p>{this.props.state.name + ' ' + this.props.state.lastName}</p>
-                <p>{this.props.state.title}</p>
-                <p>{this.props.state.number}</p>
-                <p>{this.props.state.email}</p>
-                <p>{this.props.state.location}</p>
-                <p>{this.props.state.description}</p>
+                <div className='general-details'>
+                    <h2>General Details</h2>
+                    <p>{this.props.state.name + ' ' + this.props.state.lastName}</p>
+                    <p>{this.props.state.title}</p>
+                    <p>{this.props.state.number}</p>
+                    <p>{this.props.state.email}</p>
+                    <p>{this.props.state.location}</p>
+                    <p>{this.props.state.description}</p>
+                </div>
+                <div className='containers'>
+                    <DisplayExp state={this.props.state}/>
+                </div>
             </div>
         )
     }
