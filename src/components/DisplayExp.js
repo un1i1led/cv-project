597243fd2b11597666344f024ade0a024d7af032
gil <gsqwow@gmail.com>
2023-02-03 21:38:31 -0700

    Change h2s to h3s

--- src/components/DisplayExp.js
+++ src/components/DisplayExp.js
@@ -8,11 +8,11 @@ export default class DisplayExp extends Component {
     render() {
         return(
             <div>
-                <h2>Experience</h2>
+                <h3>Experience</h3>
                 {this.props.state.experience.map(x => <div key={x.id}>
                     <p>{x.position}</p>
                 </div>)}
-                <h2>Education</h2>
+                <h3>Education</h3>
                 {this.props.state.education.map(x => <div key={x.id}>
                     <p>{x.course}</p>
                 </div>)}
