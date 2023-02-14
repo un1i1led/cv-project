0ba882a7b75b586cbf0e55efa0d551c71a297ccb
gil <gsqwow@gmail.com>
2023-02-14 04:32:37 -0700

    Remove addEdu and addExp function

--- src/App.js
+++ src/App.js
@@ -1,52 +1,32 @@
-import React from 'react';
+import {React, useState} from 'react';
 import Creator from './components/Creator';
 import Display from './components/Display';
 import './style.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
 
-    this.state = {
-        name: 'John',
-        lastName: 'Smith',
-        title: 'Software Engineer',
-        number: '202-555-0162',
-        email: 'myemail@email.com',
-        location: 'City, State',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dapibus, arcu in sodales pellentesque, justo enim facilisis elit, nec sodales urna felis vitae mauris.',
-        experience: [],
-        education: [],
-    }
-
-    this.updateState = this.updateState.bind(this);
-    this.addExp = this.addExp.bind(this);
-    this.addEdu = this.addEdu.bind(this);
+  const [state, setState] = useState({
+    name: 'John',
+    lastName: 'Smith',
+    title: 'Software Engineer',
+    number: '202-555-0162',
+    email: 'myemail@gmail.com',
+    location: 'City, State',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dapibus, arcu in sodales pellentesque, justo enim facilisis elit, nec sodales urna felis vitae mauris.',
+    experience: [],
+    education: []
+  });
+  
+  const updateState = (state, value) => {
+    setState(prevState => ({...prevState, [state]: value}));
   }
 
-  updateState = (state, value) => this.setState({[state]: value});
-
-  addExp = (expItem) => {
-    this.setState({
-      experience: [...this.state.experience, expItem]
-    });
-  }
-
-  addEdu = (eduItem) => {
-    this.setState({
-      education: [...this.state.education, eduItem]
-    });
-  }
-
-  render() {
-    return(
-      <div className='main'>
-        <Creator state={this.state} updateState={this.updateState} 
-        addExp={this.addExp} addEdu={this.addEdu}/>
-        <Display state={this.state}/>
-      </div>
-    )
-  }
+  return (
+    <div className='main'>
+      <Creator state={state} updateState={updateState}/>
+      <Display state={state}/>
+    </div>
+  )
 }
 
 export default App;
